a1652f53fd7c7852509d58e083da5777d80ad4d7
hikar1-07 <81418203+hikar1-07@users.noreply.github.com>
2025-11-19 17:20:39 +0300

    Задание №3: Сделать всё то же самое, но для контактов

---
+++ contact.py
@@ -0,0 +1,26 @@
+class Contact:
+    def __init__(self,
+                 firstname,
+                 middlename,
+                 lastname,
+                 nickname,
+                 title,
+                 company,
+                 address,
+                 mobile,
+                 email,
+                 bday,
+                 bmonth,
+                 byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
--- test_add_group.py
+++ test_add_group.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
@@ -85,6 +83,7 @@ class TestAddGroup(unittest.TestCase):
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
