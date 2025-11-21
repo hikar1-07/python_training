7a06822889efe5e5bcadf5defdf06f7e8afce3f8
hikar1-07 <81418203+hikar1-07@users.noreply.github.com>
2025-11-22 01:26:26 +0300

    Оптимизация ввода данных в формы

--- test/test_modify_first_group.py
+++ test/test_modify_first_group.py
@@ -1,9 +1,17 @@
 from model.group import Group
 
 
-def test_modify_first_group(app):
+def test_modify_first_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group("New name",
-                                     "New header",
-                                     "New footer"))
+    app.group.modify_first_group(Group("New group name"))
+    app.session.logout()
+
+def test_modify_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New group header"))
+    app.session.logout()
+
+def test_modify_first_group_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(footer="New group footer"))
     app.session.logout()
