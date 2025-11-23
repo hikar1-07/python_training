84fc3a1ab8789864512e3979aaf5098a56f441fb
hikar1-07 <81418203+hikar1-07@users.noreply.github.com>
2025-11-23 23:31:55 +0300

    Задание №11: Реализовать проверки в виде сравнения списков

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,15 +4,25 @@ from conftest import app
 
 
 def test_add_contact(app):
-    app.contact.create_contact(
-        Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="test_user_ivan",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="test_user_ivan",
                 title="test_user", company="ABC", address="ABC city", home="ABC", mobile="70000000000",
                 work="71000000000", fax="72000000000",
                 email="test@testtest.com", email2="test2@testtest.com", email3="test3@testtest.com",
-                homepage="www.ya.ru", bday="9", bmonth="July", byear="1970", aday="8", amonth="June", ayear="1999"))
+                homepage="www.ya.ru", bday="9", bmonth="July", byear="1970", aday="8", amonth="June", ayear="1999")
+    app.contact.create_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 def test_add_empty_contact(app):
-    app.contact.create_contact(
-        Contact(firstname="", middlename="", lastname="", nickname="",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="", middlename="", lastname="", nickname="",
                 title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="",
-                email3="", homepage="", bday="", bmonth="-", byear="", aday="", amonth="-", ayear=""))
+                email3="", homepage="", bday="", bmonth="-", byear="", aday="", amonth="-", ayear="")
+    app.contact.create_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_modify_first_contact.py
+++ test/test_modify_first_contact.py
@@ -6,22 +6,36 @@ from conftest import app
 def test_modify_first_contact_all(app):
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(firstname="Testusertodelete"))
-    app.contact.modify_first_contact_all(
-        Contact("Mary", "Ivanovna", "Ivanovna", "test_user_mary",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact("Mary", "Ivanovna", "Ivanovna", "test_user_mary",
                 "test_user2", "CBA", "CBA city", "CBA", "7999000000",
                 "79980000000", "79970000000",
                 "test99@testtest.com","test98@testtest.com", "test97@testtest.com",
-                "www.yaya.com", "22", "July", "1991", "19","April", "2009"))
+                "www.yaya.com", "22", "July", "1991", "19","April", "2009")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact_all(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+def test_modify_first_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(firstname="Testusertodel"))
+    old_contacts = app.contact.get_contact_list()
+    app.contact.modify_first_contact_lastname()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    modified_contact = Contact(lastname="Zukova-Testovskaya")
+    modified_contact.id = old_contacts[0].id
+    modified_contact.firstname = old_contacts[0].firstname
+    old_contacts[0] = modified_contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 def test_modify_first_contact_phone(app):
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(firstname="Testusertodel"))
     app.contact.modify_first_contact_phone()
-
-def test_modify_first_contact_lastname(app):
-    if app.contact.count() == 0:
-        app.contact.create_contact(Contact(firstname="Testusertodel"))
-    app.contact.modify_first_contact_lastname()
 
 def test_modify_first_contact_email(app):
     if app.contact.count() == 0:
