9dade602e70835af1e37e10f77257aea99525565
hikar1-07 <81418203+hikar1-07@users.noreply.github.com>
2025-12-03 23:46:21 +0300

    Задание №19: Реализовать загрузку тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,14 +3,14 @@ from model.contact import Contact
 from conftest import app
 
 
-# def test_add_contact_generated_data(app, json_contacts):
-#     contact = json_contacts
-#     old_contacts = app.contact.get_contact_list()
-#     app.contact.create_contact(contact)
-#     assert len(old_contacts) + 1 == app.contact.count()
-#     new_contacts = app.contact.get_contact_list()
-#     old_contacts.append(contact)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+def test_add_contact_generated_data(app, json_contacts):
+    contact = json_contacts
+    old_contacts = app.contact.get_contact_list()
+    app.contact.create_contact(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
+    new_contacts = app.contact.get_contact_list()
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 def test_add_contact_default_data(app, data_contacts):
     contact = data_contacts
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,14 +3,14 @@ from model.group import Group
 from conftest import app
 
 
-# def test_add_group_generated_data(app, json_groups):
-#     group = json_groups
-#     old_groups = app.group.get_group_list()
-#     app.group.create(group)
-#     assert len(old_groups) + 1 == app.group.count()
-#     new_groups = app.group.get_group_list()
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+def test_add_group_generated_data(app, json_groups):
+    group = json_groups
+    old_groups = app.group.get_group_list()
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_group_default_data(app, data_groups):
     group = data_groups
