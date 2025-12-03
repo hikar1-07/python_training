2fa113b9a76905763ad4d2290fedb3509440b4bd
hikar1-07 <81418203+hikar1-07@users.noreply.github.com>
2025-12-03 22:58:20 +0300

    Задание №18: Реализовать генераторы тестовых данных

---
+++ data/contacts.py
@@ -0,0 +1,13 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="Mary1", middlename="Ivanovna1", lastname="Ivanov1", nickname="test_user_mary1",
+                    title="test_user1", company="ABC1", address="ABC city1", home="ABC1", mobile="70000000001",
+                    work="71000000001", fax="72000000001", email="test@testtest1.com", email2="test2@testtest1.com",
+                    email3="test3@testtest.com", homepage="www.ya.ru"),
+    Contact(firstname="Ivan2", middlename="Ivanovich2", lastname="Ivanov2", nickname="test_user_ivan2",
+                    title="test_user2", company="ABC2", address="ABC city2", home="ABC2", mobile="70000000000",
+                    work="71000000000", fax="72000000002", email="test@testtest2.com", email2="test2@testtest2.com",
+                    email3="test3@testtest2.com", homepage="www.ya.ru")
+]
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
-from string import punctuation
-
 from model.group import Group
 from conftest import app
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits +  " " * 1 #string.punctuation +
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+# def test_add_group_generated_data(app, json_groups):
+#     group = json_groups
+#     old_groups = app.group.get_group_list()
+#     app.group.create(group)
+#     assert len(old_groups) + 1 == app.group.count()
+#     new_groups = app.group.get_group_list()
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-testdata = [Group(name="",header= "", footer="")] + [
-             Group(name=random_string("group name", 5),
-                   header=random_string("group header", 10),
-                   footer=random_string("group footer", 10)) for i in range(5)
-]
-
-
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group_default_data(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
