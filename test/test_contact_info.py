5f37d05ce09a06352667553e90e9f6c22aa30de5
hikar1-07 <81418203+hikar1-07@users.noreply.github.com>
2025-12-09 00:45:28 +0300

    Задание №21: Переделать тесты для проверки информации о контактах на главной странице

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -4,32 +4,32 @@ from random import randrange
 from model.contact import Contact
 
 
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-
-def test_phones_on_home_page_random(app):
-    contacts = app.contact.get_contact_list()
-    index = randrange(len(contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-
-def test_emails_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-
-def test_phones_on_view_page(app):
-    contacts = app.contact.get_contact_list()
-    index = randrange(len(contacts))
-    contact_from_view_page = app.contact.get_contact_info_from_view_page(index)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_view_page.home == contact_from_edit_page.home
-    assert contact_from_view_page.work == contact_from_edit_page.work
-    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
-
+# def test_phones_on_home_page(app):
+#     contact_from_home_page = app.contact.get_contact_list()[0]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+#
+# def test_phones_on_home_page_random(app):
+#     contacts = app.contact.get_contact_list()
+#     index = randrange(len(contacts))
+#     contact_from_home_page = app.contact.get_contact_list()[index]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+#
+# def test_emails_on_home_page(app):
+#     contact_from_home_page = app.contact.get_contact_list()[0]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+#
+# def test_phones_on_view_page(app):
+#     contacts = app.contact.get_contact_list()
+#     index = randrange(len(contacts))
+#     contact_from_view_page = app.contact.get_contact_info_from_view_page(index)
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#     assert contact_from_view_page.home == contact_from_edit_page.home
+#     assert contact_from_view_page.work == contact_from_edit_page.work
+#     assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+#
 def test_contact_info_on_home_page(app):
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(firstname="Testusertodelete"))
@@ -42,6 +42,19 @@ def test_contact_info_on_home_page(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+def test_contacts_info_on_home_page_db(app, db):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(firstname="Testusertodelete"))
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(contacts_from_db)):
+        assert contacts_from_home_page[i].firstname == contacts_from_db[i].firstname
+        assert contacts_from_home_page[i].lastname == contacts_from_db[i].lastname
+        assert contacts_from_home_page[i].address == contacts_from_db[i].address
+        assert contacts_from_home_page[i].all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db[i])
+        assert contacts_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db[i])
+
 
 def clear(s):
     return re.sub("[- ()]", "" , s)
